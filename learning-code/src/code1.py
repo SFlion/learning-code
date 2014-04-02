378aa4db95546bd38cd86f63f7c85905b890563c
SFlion <sf0729@qq.com>
2014-04-02 18:56:12 +0800

    4/2

--- learning-code/src/code1.py
+++ learning-code/src/code1.py
@@ -1,5 +1,55 @@
 '''
 Created on 2014-4-2
-
 @author: SHENFENG
 '''
+#dictionary
+d={"ad":"d",23:"3","2":25}
+print "Hello World!"
+print d["ad"]
+print d[23]
+print d["2"]
+print d
+d[23]=45
+print d
+del d["2"]
+print d
+d.clear()
+print d
+
+
+#list
+li=["sds",23,'e3']
+print li
+print li[2]
+print li[-2]
+print li[0:2]
+print li[0:-2]
+li.append(34)
+print li 
+li.insert(3, 'df')
+print li
+li.extend([23,'df'])
+print li
+print li.index(23)
+print 3 in li
+print 23 in li
+print li[0:0]
+print li.pop()
+print li*3
+print li
+print li+li
+li+=li
+print li
+
+#tuple
+t=(34,'df')
+print t
+pu={t:34}
+print pu
+tt=(li)
+print tt
+print li==tt
+
+
+
+
