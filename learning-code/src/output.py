b1e7419d5dbb29ccf81ccac323d5b67b0b87c58f
SFlion <sf0729@qq.com>
2014-04-08 11:02:04 +0800

    output

--- learning-code/src/output.py
+++ learning-code/src/output.py
@@ -6,6 +6,7 @@ Created on 2014-4-8
 import statsout
 
 def output(data,form="text"):
-    output_function=getattr(statsout,"statsout_%s"%form)
+    output_function=getattr(statsout,
+                            "statsout_%s"%form,statsout.statsout_text)
     return output_function(data)
 
