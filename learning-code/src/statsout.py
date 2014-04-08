35e387fdca208fdcb6d5299d8d9fad6d74bf89b8
SFlion <sf0729@qq.com>
2014-04-08 10:33:47 +0800

    statsout

---
+++ learning-code/src/statsout.py
@@ -0,0 +1,13 @@
+'''
+Created on 2014-4-8
+
+@author: SHENFENG
+'''
+def statsout_html(data):
+    """Format data as HTML"""
+
+def statsout_xml(data):
+    """Format data as XML"""
+
+def statsout_text(data):
+    """Format data in plain text"""
