8d13bb26e7365cf8813c125be6c3cceb43485711
SFlion <sf0729@qq.com>
2014-04-08 10:47:14 +0800

    addpath

--- learning-code/src/addgitpath.py
+++ learning-code/src/addgitpath.py
@@ -2,6 +2,7 @@
 Created on 2014-4-8
 
 @author: SHENFENG
+append the path of gitrep to the sys path
 '''
 import sys
 sys.path.append('D:\program files\python 2.7\gitreps\learning-code\learning-code\src')
